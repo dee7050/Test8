1e1f3111c695ee1fe0c5b0be1ee9e1a4d9934788
dee7050 <tdk7011@ro.ru>
2021-09-29 16:56:24 +0300

    New test

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.security.KeyStore;
+
+public class Test1 {
+
+    @Test
+    void FirstTest() {
+        Assertions.assertTrue(true);
+    }
+}
